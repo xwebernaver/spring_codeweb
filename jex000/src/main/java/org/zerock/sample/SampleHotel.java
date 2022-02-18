7edbbf71ef98ccbcd917fb8cfb9740a99c8ca646
windguy1999 <windguy1999@gmail.com>
2022-02-18 20:57:08 +0900

    first 001

---
+++ jex000/src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,22 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor
+public class SampleHotel {
+	private Chef chef;
+	
+	/*
+	 * @AllArgsConstructor 사용시 필요 없음
+	public SampleHotel(Chef chef) {
+		this.chef = chef;
+	}
+	*/
+}
